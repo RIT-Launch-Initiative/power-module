47361a6ef91bb2797fec7e1eb74073222dc39149
yevagorbachev <yevgeniy.gorbachev@gmail.com>
2023-06-15 01:16:32 -0400

    Added HardFault ITM messsage

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -87,7 +87,10 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
   /* USER CODE BEGIN HardFault_IRQn 0 */
-
+    const char hf[] = "HardFault\n";
+    for (int i = 0; i < sizeof(hf); i++) {
+        ITM_SendChar(hf[i]);
+    }
   /* USER CODE END HardFault_IRQn 0 */
   while (1)
   {
