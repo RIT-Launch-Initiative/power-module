5699386be96fc1c1ee13521e5f5879971b364f5e
Aaron Chan <aarchan108@gmail.com>
2023-07-03 23:03:50 -0600

    Put networking tasks into their own header

--- Core/Inc/networking.h
+++ Core/Inc/networking.h
@@ -1,36 +1,132 @@
-////
-//// Created by aaron on 7/2/23.
-////
-//
-//#ifndef POWER_MODULE_NETWORKING_H
-//#define POWER_MODULE_NETWORKING_H
-//
-//#include "sched.h"
-//#include "sched/macros.h"
-//
-//#include "device/platforms/stm32/HAL_SPIDevice.h"
-//#include "device/platforms/stm32/HAL_GPIODevice.h"
-//#include "device/peripherals/LED/LED.h"
-//
-////#include "device/peripherals/W5500/W5500.h"
-//#include "device/peripherals/wiznet/wiznet.h"
-//#include "net/packet/Packet.h"
-//#include "net/stack/IPv4UDP/IPv4UDPStack.h"
-//#include "net/stack/IPv4UDP/IPv4UDPSocket.h"
-//
-//extern Wiznet *w5500;
-//extern IPv4UDPStack *stack;
-//extern IPv4UDPSocket *sock;
-//
-//extern HALSPIDevice *wiz_spi;
-//extern HALGPIODevice *wiz_cs;
-//extern HALGPIODevice *wiz_rst;
-//extern HALGPIODevice *wizLEDGPIO;
-//extern LED *wiz_led;
-//
-//RetType wiz_spi_poll_task(void *);
-////RetType wizRecvTestTask(void *);
-//RetType wizSendTestTask(void *);
-//RetType netStackInitTask(void *);
-//
-//#endif //POWER_MODULE_NETWORKING_H
+/**
+ * File for all networking tasks
+ * @author Aaron Chan
+ */
+
+#ifndef POWER_MODULE_NETWORKING_H
+#define POWER_MODULE_NETWORKING_H
+
+#include "sched.h"
+#include "sched/macros.h"
+
+#include "device/platforms/stm32/HAL_SPIDevice.h"
+#include "device/platforms/stm32/HAL_GPIODevice.h"
+#include "device/peripherals/LED/LED.h"
+
+#include "device/peripherals/wiznet/wiznet.h"
+#include "net/packet/Packet.h"
+#include "net/stack/IPv4UDP/IPv4UDPStack.h"
+#include "net/stack/IPv4UDP/IPv4UDPSocket.h"
+
+/**
+ * File for all networking tasks
+ * @author Aaron Chan
+ */
+
+#include "networking.h"
+#include "device/platforms/stm32/swdebug.h"
+
+Wiznet *w5500 = nullptr;
+HALSPIDevice* wiz_spi = nullptr;
+HALGPIODevice* wiz_cs = nullptr;
+HALGPIODevice *wiz_rst = nullptr;
+HALGPIODevice *wiz_led_gpio = nullptr;
+LED *wiz_led = nullptr;
+
+IPv4UDPStack *stack = nullptr;
+IPv4UDPSocket *sock = nullptr;
+
+
+
+RetType wiz_spi_poll_task(void *) {
+    RESUME();
+    CALL(wiz_spi->poll());
+    RESET();
+    return RET_SUCCESS;
+}
+
+RetType wizRecvTestTask(void *) {
+    RESUME();
+    static Packet packet = alloc::Packet<IPv4UDPSocket::MTU_NO_HEADERS - IPv4UDPSocket::HEADERS_SIZE, IPv4UDPSocket::HEADERS_SIZE>();
+    static uint8_t *buff;
+
+    RetType ret = CALL(w5500->recv_data(stack->get_eth(), packet));
+    buff = packet.raw();
+
+    swprint(reinterpret_cast<const char *>(buff));
+
+    RESET();
+    return RET_SUCCESS;
+}
+
+RetType wizSendTestTask(void *) {
+    RESUME();
+    static IPv4UDPSocket::addr_t addr;
+    addr.ip[0] = 10;
+    addr.ip[1] = 10;
+    addr.ip[2] = 10;
+    addr.ip[3] = 96;
+    addr.port = 8000;
+
+    static uint8_t buff[7] = {'L', 'a', 'u', 'n', 'c', 'h', '!'};
+    RetType ret = CALL(sock->send(buff, 7, &addr));
+
+    RESET();
+    return RET_SUCCESS;
+}
+
+RetType netStackInitTask(void *) {
+    RESUME();
+    sched_start(wiz_spi_poll_task, {});
+
+    static Wiznet wiznet(*wiz_spi, *wiz_cs, *wiz_rst, *wiz_led);
+    w5500 = &wiznet;
+
+    static IPv4UDPStack iPv4UdpStack{10, 10, 10, 69, \
+                              255, 255, 255, 0,
+                                     *w5500};
+    stack = &iPv4UdpStack;
+
+    static uint8_t ip_addr[4] = {192, 168, 1, 10};
+    static uint8_t subnet_mask[4] = {255, 255, 255, 0};
+    static uint8_t gateway_addr[4] = {192, 168, 1, 1};
+    static uint8_t mac_addr[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    static IPv4UDPSocket::addr_t addr;
+
+    sock = stack->get_socket();
+    addr.ip[0] = addr.ip[1] = addr.ip[2] = addr.ip[3] = 0;
+    addr.port = 8000;
+    sock->bind(addr); // TODO: Error handling
+
+    ipv4::IPv4Addr_t temp_addr;
+    ipv4::IPv4Address(10, 10, 10, 69, &temp_addr);
+    stack->add_multicast(temp_addr);
+
+
+    swprint("Initializing W5500\n");
+    RetType ret = CALL(wiznet.init(mac_addr));
+    if (RET_SUCCESS != ret) {
+		swprint("#RED#W5500 init failed\n");
+        goto netStackInitDone;
+    } else {
+		swprint("#GRN#W5500 init OK\n");
+    }
+
+
+    swprint("Initializing network stack\n");
+    ret = stack->init();
+    if (RET_SUCCESS != ret) {
+    	swprint("#RED#Net stack init failed");
+        goto netStackInitDone;
+    } else {
+		swprint("#GRN#Net stack init OK\n");
+    }
+
+    swprint("Successfully initialized network interface\n");
+//    sched_start(wizSendTestTask, {});
+    sched_start(wizRecvTestTask, {});
+    netStackInitDone:
+    RESET();
+    return RET_ERROR; // Kill task
+}
+#endif //POWER_MODULE_NETWORKING_H
--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -29,14 +29,7 @@
 #include "device/peripherals/LED/LED.h"
 
 #include "sched/macros.h"
-
-
-
-#include "device/peripherals/wiznet/wiznet.h"
-#include "net/packet/Packet.h"
-#include "net/stack/IPv4UDP/IPv4UDPStack.h"
-#include "net/stack/IPv4UDP/IPv4UDPSocket.h"
-//#include "networking.h"
+#include "networking.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -68,19 +61,6 @@ static HALI2CDevice *i2c = nullptr;
 static HALSPIDevice *w25q_spi = nullptr;
 static HALGPIODevice *w25q_cs = nullptr;
 
-static Wiznet *w5500 = nullptr;
-static HALSPIDevice* wiz_spi = nullptr;
-static HALGPIODevice* wiz_cs = nullptr;
-static HALGPIODevice *wiz_rst = nullptr;
-static HALGPIODevice *wiz_led_gpio = nullptr;
-static LED *wiz_led = nullptr;
-
-static IPv4UDPStack *stack = nullptr;
-static IPv4UDPSocket *sock = nullptr;
-
-
-
-
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -111,92 +91,11 @@ RetType i2c_poll_task(void *) {
 RetType spi_poll_task(void *) {
     RESUME();
     CALL(w25q_spi->poll());
-    CALL(wiz_spi->poll());
     RESET();
     return RET_SUCCESS;
 }
 
-RetType wizRecvTestTask(void *) {
-    RESUME();
-    static Packet packet = alloc::Packet<IPv4UDPSocket::MTU_NO_HEADERS - IPv4UDPSocket::HEADERS_SIZE, IPv4UDPSocket::HEADERS_SIZE>();
-    static uint8_t *buff;
 
-    RetType ret = CALL(w5500->recv_data(stack->get_eth(), packet));
-    buff = packet.raw();
-
-    RESET();
-    return RET_SUCCESS;
-}
-
-RetType wizSendTestTask(void *) {
-    RESUME();
-    static IPv4UDPSocket::addr_t addr;
-    addr.ip[0] = 10;
-    addr.ip[1] = 10;
-    addr.ip[2] = 10;
-    addr.ip[3] = 96;
-    addr.port = 8000;
-
-    static uint8_t buff[7] = {'L', 'a', 'u', 'n', 'c', 'h', '!'};
-    RetType ret = CALL(sock->send(buff, 7, &addr));
-
-    RESET();
-    return RET_SUCCESS;
-}
-
-RetType netStackInitTask(void *) {
-    RESUME();
-
-    static Wiznet wiznet(*wiz_spi, *wiz_cs, *wiz_rst, *wiz_led);
-    w5500 = &wiznet;
-
-    static IPv4UDPStack iPv4UdpStack{10, 10, 10, 69, \
-                              255, 255, 255, 0,
-                                     *w5500};
-    stack = &iPv4UdpStack;
-
-    static uint8_t ip_addr[4] = {192, 168, 1, 10};
-    static uint8_t subnet_mask[4] = {255, 255, 255, 0};
-    static uint8_t gateway_addr[4] = {192, 168, 1, 1};
-    static uint8_t mac_addr[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
-    static IPv4UDPSocket::addr_t addr;
-
-    sock = stack->get_socket();
-    addr.ip[0] = addr.ip[1] = addr.ip[2] = addr.ip[3] = 0;
-    addr.port = 8000;
-    sock->bind(addr); // TODO: Error handling
-
-    ipv4::IPv4Addr_t temp_addr;
-    ipv4::IPv4Address(10, 10, 10, 69, &temp_addr);
-    stack->add_multicast(temp_addr);
-
-
-    swprint("Initializing W5500\n");
-    RetType ret = CALL(wiznet.init(mac_addr));
-    if (RET_SUCCESS != ret) {
-		swprint("#RED#W5500 init failed\n");
-        goto netStackInitDone;
-    } else {
-		swprint("#GRN#W5500 init OK\n");
-    }
-
-
-    swprint("Initializing network stack\n");
-    ret = stack->init();
-    if (RET_SUCCESS != ret) {
-    	swprint("#RED#Net stack init failed");
-        goto netStackInitDone;
-    } else {
-		swprint("#GRN#Net stack init OK\n");
-    }
-
-    swprint("Successfully initialized network interface\n");
-    sched_start(wizSendTestTask, {});
-
-    netStackInitDone:
-    RESET();
-    return RET_ERROR; // Kill task
-}
 
 
 
