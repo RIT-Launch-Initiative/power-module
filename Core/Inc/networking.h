038e95ab4b8fcbe104636d26be29e916bd012087
Aaron Chan <aarchan108@gmail.com>
2023-08-15 23:55:53 -0400

    Update networking initializations

--- Core/Inc/networking.h
+++ Core/Inc/networking.h
@@ -7,6 +7,7 @@
 #define POWER_MODULE_NETWORKING_H
 
 #include "sched.h"
+#include "init/init.h"
 #include "sched/macros.h"
 
 #include "device/platforms/stm32/HAL_SPIDevice.h"
@@ -17,11 +18,6 @@
 #include "net/packet/Packet.h"
 #include "net/stack/IPv4UDP/IPv4UDPStack.h"
 #include "net/stack/IPv4UDP/IPv4UDPSocket.h"
-
-/**
- * File for all networking tasks
- * @author Aaron Chan
- */
 
 #include "networking.h"
 #include "device/platforms/stm32/swdebug.h"
@@ -45,27 +41,13 @@ RetType wiz_spi_poll_task(void *) {
     return RET_SUCCESS;
 }
 
-RetType wizRecvTestTask(void *) {
-    RESUME();
-    static Packet packet = alloc::Packet<IPv4UDPSocket::MTU_NO_HEADERS - IPv4UDPSocket::HEADERS_SIZE, IPv4UDPSocket::HEADERS_SIZE>();
-    static uint8_t *buff;
-
-    RetType ret = CALL(w5500->recv_data(stack->get_eth(), packet));
-    buff = packet.raw();
-
-    swprint(reinterpret_cast<const char *>(buff));
-
-    RESET();
-    return RET_SUCCESS;
-}
-
 RetType wizSendTestTask(void *) {
     RESUME();
     static IPv4UDPSocket::addr_t addr;
     addr.ip[0] = 10;
     addr.ip[1] = 10;
     addr.ip[2] = 10;
-    addr.ip[3] = 96;
+    addr.ip[3] = 69;
     addr.port = 8000;
 
     static uint8_t buff[7] = {'L', 'a', 'u', 'n', 'c', 'h', '!'};
@@ -79,19 +61,19 @@ RetType netStackInitTask(void *) {
     RESUME();
     sched_start(wiz_spi_poll_task, {});
 
-    static Wiznet wiznet(*wiz_spi, *wiz_cs, *wiz_rst, *wiz_led);
-    w5500 = &wiznet;
-
-    static IPv4UDPStack iPv4UdpStack{10, 10, 10, 69, \
-                              255, 255, 255, 0,
-                                     *w5500};
-    stack = &iPv4UdpStack;
-
     static uint8_t ip_addr[4] = {192, 168, 1, 10};
     static uint8_t subnet_mask[4] = {255, 255, 255, 0};
     static uint8_t gateway_addr[4] = {192, 168, 1, 1};
     static uint8_t mac_addr[6] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
     static IPv4UDPSocket::addr_t addr;
+
+    static Packet packet = alloc::Packet<IPv4UDPSocket::MTU_NO_HEADERS - IPv4UDPSocket::HEADERS_SIZE, IPv4UDPSocket::HEADERS_SIZE>();
+
+    static Wiznet wiznet(*wiz_spi, *wiz_cs, *wiz_rst, *wiz_led_gpio, stack->get_eth(), packet);
+    w5500 = &wiznet;
+
+    static IPv4UDPStack iPv4UdpStack(10, 10, 10, 69, 255, 255, 255, 0, wiznet);
+    stack = &iPv4UdpStack;
 
     sock = stack->get_socket();
     addr.ip[0] = addr.ip[1] = addr.ip[2] = addr.ip[3] = 0;
@@ -102,29 +84,25 @@ RetType netStackInitTask(void *) {
     ipv4::IPv4Address(10, 10, 10, 69, &temp_addr);
     stack->add_multicast(temp_addr);
 
-
-    swprint("Initializing W5500\n");
-    RetType ret = CALL(wiznet.init(mac_addr));
-    if (RET_SUCCESS != ret) {
-		swprint("#RED#W5500 init failed\n");
+    RetType ret = CALL(wiznet.init());
+    if (ret != RET_SUCCESS) {
+        swprint("#RED#W5500 init failed\n");
         goto netStackInitDone;
     } else {
 		swprint("#GRN#W5500 init OK\n");
     }
 
-
-    swprint("Initializing network stack\n");
-    ret = stack->init();
-    if (RET_SUCCESS != ret) {
-    	swprint("#RED#Net stack init failed");
+    if (RET_SUCCESS != stack->init()) {
+        swprint("#RED#Net stack init failed");
         goto netStackInitDone;
     } else {
 		swprint("#GRN#Net stack init OK\n");
     }
 
     swprint("Successfully initialized network interface\n");
-//    sched_start(wizSendTestTask, {});
-    sched_start(wizRecvTestTask, {});
+    tid_t tid = sched_start(PollDevice, (void *) w5500);
+    sched_start(wizSendTestTask, {});
+
     netStackInitDone:
     RESET();
     return RET_ERROR; // Kill task
